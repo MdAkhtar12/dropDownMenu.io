ead436aca1952f1ff0e48ad0c00795969533dcea
Md Shahnewaz Akhtar <shahnawazakhtar1998@gmail.com>
2023-10-27 00:42:01 +0530

    dropdownmenu

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigarionEl = document.querySelector(".navigarion");
+let toggleMenuEl = document.querySelector(".toggleMenu");
+
+toggleMenuEl.onclick = function(){
+    navigarionEl.classList.toggle("active")
+}
